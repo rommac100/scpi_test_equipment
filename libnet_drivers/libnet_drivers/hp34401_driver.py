3448f0f1857d2bd4af2e1b91cba7fa2e9c6eb906
rommac <rommac@lab-server.rommac.com>
2024-04-06 19:49:41 -0700

    CAM: updated libnet drivers

--- libnet_drivers/libnet_drivers/hp34401_driver.py
+++ libnet_drivers/libnet_drivers/hp34401_driver.py
@@ -2,22 +2,38 @@ from libnet_wrapper import libnet_wrapper
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 class hp34401_driver:
     def __init__(self, host, addr):
         self.host = host
         self.addr = addr
         self.gpib_driver = libnet_wrapper.GPIBDriver(self.host,self.addr)
-    def query(self,command_str):
-        return self.gpib_driver.query(command_str.encode("UTF-8"))
+    def query(self,command_str,delay=.2):
+        self.write(command_str)
+        time.sleep(delay)
+        return self.read()
     def read(self):
-        return self.gpib_driver.read()
+        return self.gpib_driver.read().decode()
     def write(self,command_str):
         self.gpib_driver.write(command_str.encode("UTF-8"))
+    def meas_dc_voltage(self):
+        return self.query("MEASure:VOLTage:DC?")
+    def meas_dc_current(self):
+        return float(self.query("MEASURE:CURRENT:DC?"))
+    def get_configuration(self):
+        return self.query("CONFIGURE?")
+    def get_error_list(self):
+        return self.query("SYSTEM:ERRor?")
+    def get_measurement(self):
+        return self.query("MEASURE?")
     def get_idn_str(self):
-        idn_str = self.query("*IDN?").decode()
+        idn_str = self.query("*IDN?")
         print(idn_str)
         return idn_str
+    
 if __name__ == "__main__":
     hp34401 = hp34401_driver("10.2.0.9",22)
     hp34401.get_idn_str()
+    print(hp34401.get_error_list())
+    print(hp34401.meas_dc_voltage())
--- libnet_drivers/libnet_drivers/hp6632_driver.py
+++ libnet_drivers/libnet_drivers/hp6632_driver.py
@@ -2,6 +2,7 @@ from libnet_wrapper import libnet_wrapper
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 class hp6632_driver:
     def __init__(self, host, addr):
@@ -9,33 +10,30 @@ class hp6632_driver:
         self.addr = addr
         self.gpib_driver = libnet_wrapper.GPIBDriver(self.host,self.addr)
     def query(self,command_str):
-        return self.gpib_driver.query(command_str.encode("UTF-8"))
+        return self.gpib_driver.query(command_str.encode("UTF-8")).decode()
     def read(self):
-        return self.gpib_driver.read()
+        return self.gpib_driver.read().decode()
     def write(self,command_str):
         self.gpib_driver.write(command_str.encode("UTF-8"))
     def get_idn_str(self):
-        idn_str = self.query("ID?").decode()
+        idn_str = self.query("ID?")
         print(idn_str)
         return idn_str
-    def select_output(self,output_number):
-        self.write("INSTRUMENT:NSELECT %d"%output_number)
-    def get_selected_output(self):
-        return self.query("INSTR:NSEL?")
     def set_output_on(self):
-        self.write("OUTPUT:STATE ON")
+        self.write("OUT 1")
     def set_output_off(self):
-        self.write("OUTPUT:STATE OFF")
-    def get_output_state(self):
-        return self.query("OUTPUT:STATE?")
+        self.write("OUT 0")
     def set_output_voltage(self,volt):
-        self.write("SOURCE:VOLTAGE %.2f"%volt)
-    def get_output_voltage(self):
-        return self.query("SOURCE:VOLT?")
+        self.write("VSET %.2f"%volt)
+    def set_output_current(self,current):
+        self.write("ISET %.2f"%current)
     def meas_curr_voltage(self):
-        return self.query("MEASURE:VOLTAGE?")
+        return self.query("VOUT?")
     def meas_curr_current(self):
-        return self.query("MEASURE:CURRENT?")
+        return self.query("IOUT?")
 if __name__ == "__main__":
     hp6632 = hp6632_driver("10.2.0.9",6)
     hp6632.get_idn_str()
+    hp6632.set_output_on()
+    hp6632.set_output_voltage(2.5)
+    print(hp6632.meas_curr_voltage())
