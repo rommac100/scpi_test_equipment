de81fb5dc6d973c078ff563cc95fae18037e0054
Christopher <rommac100@gmail.com>
2024-08-11 18:32:38 -0700

    CAM: commiting basic tds drivers

--- libnet_drivers/libnet_drivers/tds5054b_driver.py
+++ libnet_drivers/libnet_drivers/tds5054b_driver.py
@@ -21,30 +21,49 @@ class tds5054b_driver:
         return self.gpib_driver.read()
     def write(self,command_str):
         self.gpib_driver.write(command_str.encode("UTF-8"))
-    def read_binary_data(self,length):
-        data = self.read()
-        data_out = []
-        print('test')
-        while (len(data) < data_out):
-           data_out.append(data)
-           data = self.read()
+        #return data_out
+    def read_waveform_data(self):
+        print('reading waveform data')
+        self.write("DATA:SOURCE CH1")
+        self.write("DATA:ENCDG ASCII")
+        print(self.query('DATA:STOP?'))
+        print(self.query('DATA:START?'))
+        print(self.query("DATA?"))
 
-        return data_out
+        print(self.query("CURVE?"))
+        data_list = []
+        data = self.read()
+        while (len(data_list) < 5001):
+            print(data)
+            data_list.append(data)
+            data = self.read()
+
+
+    def set_measurement_pk_pk(self,channel):
+        print('reading measurement data')
+
+    def set_measurement_mean(self,channel):
+        print('reading measurement data')
+
+
+    def read_measurement_data(self):
+        print('reading measurement data')
     def transfer_hard_copy(self,filename):
+        self.write('*CLS')
+        self.write('FILESYSTEM:CWD "C:\\TekScope\\images"')
         self.write('HARDCOPY:PORT FILE')
-        self.write('HARDCOPY:FILENAME "C:\TekScope\images\TekH001.PNG"')
+        self.write('HARDCOPY:FILENAME "C:\\TekScope\\images\\TekH001.PNG"')
         self.write('HARDCOPY START')
         self.query('*OPC?')
         time.sleep(3)
-        self.write('FILESYSTEM:READFILE "C:\TekScope\images\TekH001.PNG"')
-        data = self.read_binary_data(640*480)
-        print(data)
+        self.write('FILESYSTEM:PRINT "C:\\TekScope\\images\\TekH001.PNG",GPIB')
+        for ii in range(50):
+            print(self.read())
 
 
     # returns int array
 if __name__ == "__main__":
     tds5054b = tds5054b_driver("10.2.0.9",26)
-    tds5054b.write("*CLS")
-    print(tds5054b.query("HARDCOPY?"))
-    #tds5054b.get_idn_str()
-    tds5054b.transfer_hard_copy("test")
+    tds5054b.get_idn_str()    
+    tds5054b.read_waveform_data()
+    #tds5054b.transfer_hard_copy("test")
